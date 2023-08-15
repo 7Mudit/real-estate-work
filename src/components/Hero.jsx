5a3276a6cb355b75b4980cae73765dafe2f331d9
Mudit Kapoor <muditk441@gmail.com>
2023-08-15 18:46:52 +0530

    testing front page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <section id="home" className={`flex md:flex-row  flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 ">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">20%</span> Discount For{" "}
@@ -15,22 +15,20 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px]  text-[22px] text-white ss:leading-[100.8px] sm:leading-[75px]">
+            Sell Your Property in
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Real Estate Agent.
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[22px] text-white 
+        ss:leading-[100.8px] w-full">
+           <span className="text-gradient ss:text-[80px] text-[35px]">Canberra</span> <br /> with confidence !!.
         </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+        <p className={`${styles.paragraph} text-justify max-w-[470px] mt-5`}>
+        Our trusted team of expert real estate agents provide personalized marketing strategies and transparent communication to maximize the value of your property.
         </p>
       </div>
 
